ec919849fa4dbf7d6008f45b34b8aaac35c9b91a
Sphesihle <27880400@sun.ac.za>
2025-11-04 20:22:07 +0200

    Finished Hero

--- afritech-software-solutions/src/components/Header/Header.tsx
+++ afritech-software-solutions/src/components/Header/Header.tsx
@@ -5,15 +5,19 @@ export const Header = () => {
 
   ]
   return (
-    <header className="fixed z-50 border-b  backdrop-blur-lg" style={{ background: "hsl(var(--background) / 0.95)"}}>
-      <nav className="h-64 flex items-center justify-between px-4 m-0">
-        <ul className="list-none font-medium ">
-          {HeaderSections.map((item)=>(
-            <li className="bg-amber-400 ">
-            {item.section}
-          </li>))
-          }
-        </ul>
+    <header className="z-50 flex-row border-b  backdrop-blur-lg  px-6 " style={{ background: "hsl(0 0% 100% / 0.95)"
+}}>
+     
+      
+      <nav className="flex  flex-row justify-end items-center  m-0">
+        <h1 className="text-3xl flex-1 font-bold items-center " style={{ color: 'hsl(200, 95%, 35%)',backgroundColor: 'linear-gradient(to right, hsl(200, 95%, 35%), hsl(195, 85%, 50%))' }}>Afritech</h1>
+          <ul className="list-none font-medium flex  flex-row  gap-4  ">
+            {HeaderSections.map((item)=>(
+              <li className=" ml-6 rounded-2xl p-4"  style={{color:"red"}}>
+              {item.section}
+            </li>))
+            }
+          </ul>
 
       </nav>
     </header>
---
+++ afritech-software-solutions/src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import heroImage from './../../assets/hero-image.jpg';
+
+
+
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col py-8  text-center min-h-screen " style={{backgroundImage: `url(${heroImage})`,backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',}}>
+        <h1 className="text-center mb-8 mt-4 text-8xl  "style={{color:'white'}}>AfriTech Software Solutions</h1>
+        <p className="mb-8 text-4xl leading-relaxed" style={{color:'white'}}>We turn ideas into powerful, reliable web applications. Based in Cape <br/>Town, our team builds full-stack systems for businesses of all sizes,<br/> delivering fast, scalable, and production-ready software.
+        </p>
+        <p className="text-4xl mb-10" style={{color:'white'}}>Looking to modernize your business with a custom web app? Let's build it together.
+        </p>
+       <button className="py-4 px-6 text-lg bg-amber-400 hover:bg-amber-500 transition-colors rounded-md w-fit mx-auto">
+            Let's Talk
+        </button>
+
+    </div>
+  )
+}
