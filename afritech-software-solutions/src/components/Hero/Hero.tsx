d1dc43e8035d596219fa7c4fabc118a434c241f1
Sphesihle <27880400@sun.ac.za>
2025-11-10 11:24:40 +0200

    Done adding the dependencies

--- afritech-software-solutions/src/components/Hero/Hero.tsx
+++ afritech-software-solutions/src/components/Hero/Hero.tsx
@@ -8,10 +8,10 @@ export default function Hero() {
     backgroundPosition:"center"
     }}>
       <div className='relative z-10 py-32 px-4 m-0 text-center'>
-        <h1 className="mb-6 text-primary-foreground  animate-fade-in font-bold text-5xl  md:text-6xl lg:text-7xl    ">AfriTech Software Solutions</h1>
+        <h1 className="mb-6 text-primary-foreground  animate-fade-in font-bold text-5xl  md:text-6xl lg:text-7xl">AfriTech Software Solutions</h1>
         <p className="mb-8 text-xl max-w-3xl text-primary-foreground leading-relaxed bg-background-90 md:2xl  mx-auto">We turn ideas into powerful, reliable web applications. Based in Cape <br/>Town, our team builds full-stack systems for businesses of all sizes,<br/> delivering fast, scalable, and production-ready software.</p>
         <p className="text-lg text-primary-foreground mb-10 bg-background-80 md:text-xl" >Looking to modernize your business with a custom web app? Let's build it together.</p>
-          <button className="h-auto py-6 px-8 text-lg bg-accent text--accent-foreground rounded-md shadow-card transition-colors duration-300 :hover shadow-hover ">Let's Talk</button>
+          <button className="h-auto py-6 px-8 text-lg bg-accent text-accent-foreground rounded-md shadow-card transition-colors duration-300 :hover shadow-hover ">Let's Talk</button>
       </div>
     </section>
   )
