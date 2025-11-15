0c3fdcbacd005212713f5ee7538dac1897e0a83e
Sphesihle <27880400@sun.ac.za>
2025-11-15 09:18:02 +0200

    Header Skeleton code finish

--- Smart Planner Pro/FrontEnd/Smart-planner/src/components/Header.tsx
+++ Smart Planner Pro/FrontEnd/Smart-planner/src/components/Header.tsx
@@ -1,26 +1,32 @@
-import { PanelLeft,Sun,Moon,Bell,LogOut,PersonStanding } from "lucide-react"
+import { PanelLeft,Sun,Moon,Bell,LogOut,User } from "lucide-react"
+
 export default function Header() {
   return (
-     <header className="sticky top-0">
+
+    <header className="sticky top-0  border-b  w-[1350px]">
+      <div className="flex flex-row items-center justify-between p-2">
+        
+        {/* Left side: PanelLeft */}
         <div>
-            <div>
-                <PanelLeft/>
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <Moon/>
-                    </li>
-                    <li>
-                        <Bell/>
-                    </li>
-                    <li>
-                        <PersonStanding/>
-                    </li>
-                </ul>
-            </div>
+          <button className="rounded-xl  p-2 hover:bg-amber-500">
+            <PanelLeft />
+          </button>
         </div>
 
-     </header>
+        {/* Right side: Moon, Bell, User */}
+        <ul className="flex flex-row gap-2">
+          <li className="rounded-full bg-accent p-2 hover:bg-amber-500">
+            <Moon />
+          </li>
+          <li className="rounded-full bg-accent p-2 hover:bg-amber-500">
+            <Bell />
+          </li>
+          <li className="rounded-full bg-accent p-2 hover:bg-amber-500">
+            <User />
+          </li>
+        </ul>
+      </div>
+    </header>
+  
   )
 }
